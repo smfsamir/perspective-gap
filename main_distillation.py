d9e7f8230341e4d9a18fcddf1c6f49aef676a9fa
Farhan Samir <f.samir@mail.utoronto.ca>
2026-03-24 09:31:48 -0400

    add spacing

--- main_distillation.py
+++ main_distillation.py
@@ -736,7 +736,7 @@ def view_stratified_discourse_passages(input_file_prefix: str):
         paragraphs = load_unsupervised_article_paragraphs(article_name, input_file_prefix + "_articles")
         with open(f"data/{input_file_prefix}_inference_predictions/{article_name}", 'r') as f:
             predictions = json.load(f)
-        print(f"Article: {article_name}")
+        print(f"=====Article: {article_name}=====")
         for i, (paragraph, prediction) in enumerate(zip(paragraphs, predictions)):
             print(f"Paragraph {i+1} (Predicted label: {prediction}): {paragraph}\n")
 
